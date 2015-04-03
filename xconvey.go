c5f71e90c1c9ff9e3a4ed46e6c2cfe5476c7fec3
Michael Tiller <michael.tiller@gmail.com>
2015-04-03 14:51:14 -0400

    More useful shortcuts

--- xconvey.go
+++ xconvey.go
@@ -6,6 +6,14 @@ func NoError(c convey.C, err error) {
 	c.So(err, convey.ShouldBeNil)
 }
 
+func IsError(c convey.C, err error) {
+	c.So(err, convey.ShouldNotBeNil)
+}
+
+func IsNil(c convey.C, v interface{}) {
+	c.So(v, convey.ShouldBeNil)
+}
+
 func NotNil(c convey.C, v interface{}) {
 	c.So(v, convey.ShouldNotBeNil)
 }
@@ -16,4 +24,16 @@ func Equals(c convey.C, actual interface{}, expected interface{}) {
 
 func NotEquals(c convey.C, actual interface{}, expected interface{}) {
 	c.So(actual, convey.ShouldNotEqual, expected)
+}
+
+func Resembles(c convey.C, actual interface{}, expected interface{}) {
+	c.So(actual, convey.ShouldResemble, expected)
+}
+
+func IsTrue(c convey.C, actual bool) {
+	c.So(actual, convey.ShouldBeTrue)
+}
+
+func IsFalse(c convey.C, actual bool) {
+	c.So(actual, convey.ShouldBeFalse)
 }
